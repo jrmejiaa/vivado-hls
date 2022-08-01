dabf47e6e3111b6a3916e1cd97296c3f85b28504
ultra96 <ultra96@gmail.com>
2022-08-01 16:41:30 +0200

    init tapasco api launch file

---
+++ api/grayscale-launch.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tapasco.hpp>
+
+int main(int argc, char const *argv[])
+{
+    tapasco::Tapasco tapasco;
+
+    std::cout << "Test passed!" << std::endl;
+
+    return 0;
+}
