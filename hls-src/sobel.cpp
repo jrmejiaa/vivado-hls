32220be38d39f60fce2523f05922f8478fe91aa0
ultra96 <ultra96@gmail.com>
2022-09-13 17:08:59 +0200

    Improved combinatorical calculations

--- hls-src/sobel.cpp
+++ hls-src/sobel.cpp
@@ -1,29 +1,33 @@
 #include "sobel.h"
 
 void sobel(unsigned char input_r[WIDTH * HEIGHT], unsigned char output_r[(WIDTH-2)*(HEIGHT-2)]) {
-#pragma HLS array_partition variable=input_r block factor=32
-#pragma HLS array_reshape variable=input_r block factor=128
+//#pragma HLS array_reshape variable=input_r block factor=2
+//#pragma HLS array_partition variable=input_r block factor=32
 
-    LOOP1: for (int y = 1; y < HEIGHT-1; ++y) {
+    LOOP1: for (d_loidx y = 1; y < HEIGHT-1; ++y) {
 
-        LOOP2: for (int x = 1; x < WIDTH-1; ++x) {
-#pragma HLS pipeline II=1
+        LOOP2: for (d_loidx x = 1; x < WIDTH-1; ++x) {
+// #pragma HLS pipeline II=1
 
         	// tmp variables to avoid double read
-        	unsigned char tmp0_0 = input_r[(y-1)*WIDTH+x-1];
-        	unsigned char tmp0_3 = input_r[(y-1)*WIDTH+x+1];
-        	unsigned char tmp3_0 = input_r[(y+1)*WIDTH+x-1];
-        	unsigned char tmp3_3 = input_r[(y+1)*WIDTH+x+1];
+        	d_arval tmp0_0 = input_r[(y-1)*WIDTH+x-1];
+        	d_arval tmp0_3 = input_r[(y-1)*WIDTH+x+1];
+        	d_arval tmp3_0 = input_r[(y+1)*WIDTH+x-1];
+        	d_arval tmp3_3 = input_r[(y+1)*WIDTH+x+1];
+			d_arval tmp2_0 = input_r[y*WIDTH+x-1];
+			d_arval tmp2_3 = input_r[y*WIDTH+x+1];
+			d_arval tmp0_2 = input_r[(y-1)*WIDTH+x];
+			d_arval tmp3_2 = input_r[(y+1)*WIDTH+x];
 
-            int gx = tmp0_0 - tmp0_3 + 2*input_r[y*WIDTH+x-1]
-                     - 2*input_r[y*WIDTH+x+1] + tmp3_0 - tmp3_3;
+			d_sub tmp_a = tmp0_0 - tmp3_3;
+			d_sub tmp_b = tmp0_3 - tmp3_0;
 
-            int gy = tmp0_0 + 2*input_r[(y-1)*WIDTH+x] + tmp0_3
-                     - tmp3_0 - 2*input_r[(y+1)*WIDTH+x] - tmp3_3;
+        	d_g gx = tmp_a - tmp_b + 2*(tmp2_0 - tmp2_3);
+        	d_g gy = tmp_a + tmp_b + 2*(tmp0_2 - tmp3_2);
 
-            int sum = ((gx < 0) ? -gx : gx) + ((gy < 0) ? -gy : gy);
+        	d_sum sum = ((gx < 0) ? d_g(-gx) : gx) + ((gy < 0) ? d_g(-gy) : gy);
 
-            unsigned char result = (sum > 255) ? 255 : sum;
+        	d_io result = (sum > 255) ? d_sum(255) : sum;
 
             output_r[(y-1)*(WIDTH-2)+(x-1)] = result;
 
--- hls-src/sobel.h
+++ hls-src/sobel.h
@@ -6,4 +6,13 @@
     #define HEIGHT 512
 #endif
 
+#include "ap_int.h"
+
+typedef ap_uint<8> d_io;
+typedef ap_uint<9> d_loidx;
+typedef ap_uint<8> d_arval;
+typedef ap_int<9>  d_sub;
+typedef ap_int<11> d_g;
+typedef ap_uint<11> d_sum;
+
 void sobel(unsigned char input[WIDTH * HEIGHT], unsigned char output[(WIDTH - 2) * (HEIGHT - 2)]);
